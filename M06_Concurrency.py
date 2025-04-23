9cace5818e36c04f52ea62024b188452b27c002f
laguilera5 <lexusaguilera@gmail.com>
2025-04-23 09:19:23 -0500

    Update M06_Concurrency.py

--- M06_Concurrency.py
+++ M06_Concurrency.py
@@ -15,3 +15,26 @@ with open('today.txt', 'r') as file:
 #13.3
 parsed_date = datetime.datetime.strptime(today_string, '%Y-%m-%d').date()
 print(parsed_date)
+break
+
+#15.1
+import multiprocessing
+import time
+import random
+from datetime import datetime
+
+def show_time():
+    wait_time = random.random()
+    time.sleep(wait_time)
+    print(f"Process {multiprocessing.current_process().name} - Time: {datetime.now().time()}")
+
+if __name__ == '__main__':
+    processes = []
+
+    for i in range(3):
+        p = multiprocessing.Process(target=show_time, name=f'Process-{i+1}')
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
